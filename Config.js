c70f605096ab5674971f25b2a4c6cffa17f436ab
zami.tulande <zami.tulande@cariai.com>
2026-06-10 14:16:24 -0500

    boton whatsapp

--- Config.js
+++ Config.js
@@ -172,7 +172,9 @@ export var Config = (function () {
         defaultCustomization.initialLoadingAnimationCustomization.messageTextColor = "#ffffff";                // texto blanco
         defaultCustomization.initialLoadingAnimationCustomization.messageFont      = "700 22px 'Roboto', sans-serif"; // texto grande y bold
 
-        
+        // Ocultar el tag "This App is in Development Mode"
+        defaultCustomization.FaceTecCustomization.enableDevelopmentModeTag = false;
+
         return defaultCustomization;
     };
 
